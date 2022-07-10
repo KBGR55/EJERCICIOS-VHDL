e54672f22a195a539f8939c310107f1aeda25de7
K.G <100962601+KBGR55@users.noreply.github.com>
2022-07-09 21:20:45 -0500

    Update Replica_10_reg.vhd

--- EjerciciosParte_01/Replica_10_reg.vhd
+++ EjerciciosParte_01/Replica_10_reg.vhd
@@ -6,15 +6,6 @@
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
-
--- Uncomment the following library declaration if instantiating
--- any Xilinx primitives in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
-
 entity Replica_10_reg is
     Port ( clk : in  STD_LOGIC;
            D : in  STD_LOGIC_VECTOR (0 to 7);
